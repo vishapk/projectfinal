dacdfb1660f1434bbf8f0c629e175cd09c8db2cf
vishapk <53316707+vishapk@users.noreply.github.com>
2021-05-25 13:57:12 +0530

    Update Footer.js

--- project/src/components/Footers/Footer.js
+++ project/src/components/Footers/Footer.js
@@ -58,7 +58,7 @@ export default function Footer() {
                     <li>
                       <a
                         className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="https://blog.creative-tim.com?ref=nr-footer"
+                        href="https://www.sakshitantak.ml/"
                       >
                         Blog
                       </a>
